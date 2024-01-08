ca007bff264f1b3db6bc837f332fbd51d0ba1368
NIkita Chernykh <chernykhmain@gmail.com>
2024-01-08 19:45:50 +0400

    Prepare the project + carousel component placeholder

---
+++ src/components/carousel/carousel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Carousel() {
+  return (
+    <section>
+      <ul>
+        <li>Slide</li>
+        <li>Slide</li>
+        <li>Slide</li>
+        <li>Slide</li>
+        <li>Slide</li>
+      </ul>
+    </section>
+  );
+}
