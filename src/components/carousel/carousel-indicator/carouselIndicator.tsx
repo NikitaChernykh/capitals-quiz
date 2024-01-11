1ce4f62c7d68657e1f4987a7cd82d5358d76b791
NIkita Chernykh <chernykhmain@gmail.com>
2024-01-11 22:32:42 +0400

    Update carousel indicator

--- src/components/carousel/carousel-indicator/carouselIndicator.tsx
+++ src/components/carousel/carousel-indicator/carouselIndicator.tsx
@@ -10,14 +10,14 @@ const CarouselIndicator = ({
   activeSlide,
 }: CarouselIndicatorProps) => {
   return (
-    <div className='fixed lg:h-screen lg:w-10 z-40'>
+    <div className='fixed z-40 ml-1 lg:h-screen lg:w-12 lg:ml-2'>
       <div className='flex h-screen items-center'>
         <div className='flex flex-col bg-slate-500 bg-opacity-25 rounded-lg'>
           {Array.from({ length: numberOfSlides }).map((_, index) => {
             const isActive = index === activeSlide;
             const buttonClass = isActive
-              ? 'h-3 w-3 rounded-full m-2 bg-indigo-500 border-white border-2'
-              : 'h-3 w-3 bg-white rounded-full m-2';
+              ? 'h-4 w-4 rounded-full m-2 bg-indigo-500 border-white border-2'
+              : 'h-4 w-4 bg-white rounded-full m-2';
             return <span key={index} className={buttonClass}></span>;
           })}
         </div>
